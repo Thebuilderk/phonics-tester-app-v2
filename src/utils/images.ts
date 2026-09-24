0b46c21d31e603ff07e6224d9bd943ca143a8576
Kevin <test@example.com>
2026-09-25 00:27:41 +0100

    Remove citation tags from images.ts

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,13 +1,10 @@
-// src/utils/images.ts
-
 export const LOCAL_IMAGES = {
   // Images inside assets/images/
-  cat: require('../../assets/images/cat_image.png'),[cite: 27, 33]
-  dog: require('../../assets/images/dog_image.png'),[cite: 27, 33]
+  cat: require('../../assets/images/cat_image.png'),
+  dog: require('../../assets/images/dog_image.png'),
 
   // Images directly in assets/
-  fox: require('../../assets/avatar_fox.png'),[cite: 26, 28]
-  owl: require('../../assets/avatar_owl.png'),[cite: 26, 28]
-  robot: require('../../assets/avatar_robot.png'),[cite: 26, 28]
-  sunLogo: require('../../assets/sun_logo.png'),[cite: 26, 29]
+  sun: require('../../assets/sun_logo.png'),
+  fox: require('../../assets/avatar_fox.png'),
+  owl: require('../../assets/avatar_owl.png'),
 };
